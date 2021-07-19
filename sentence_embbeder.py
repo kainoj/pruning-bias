3681bbeded4f880a51943c58818a510dcb9919f3
Przmek <kainoj@users.noreply.github.com>
2021-07-19 21:54:49 +0900

    docstringed sentence embedder

--- sentence_embbeder.py
+++ sentence_embbeder.py
@@ -1,21 +1,29 @@
+from typing import List
 import torch
-
-from seat import SEAT6, SEAT7, SEAT8
-
 
 class SentenceEmbedder():
 
-    def __init__(self, model, tokenizer, mode='CLS') -> None:
+    def __init__(self, model_name: str, mode: str='CLS') -> None:
+        """Wrapper for 🤗's pipeline abstraction with sentence representation.
 
-        self.model = model
-        self.tokenizer = tokenizer
+        Args:
+            model_name: e.g.: `distilbert-base-uncased`
+            mode: how to represent sentence given tokens embeddings? 
+                'CLS': sentence representation as embedding of [CLS] token
+                 (taken at the last hidden state)
+        """
 
         if mode != 'CLS':
             raise NotImplementedError()
+        
+        self.model = AutoModel.from_pretrained(model_name)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-    def __call__(self, x) -> torch.tensor:
 
-        inputs = self.tokenizer(x, return_tensors="pt", padding=True)
+    def __call__(self, sentences: List[str]) -> torch.tensor:
+
+        # Tokenizer does automatically prepend [CLS] to sentences.
+        inputs = self.tokenizer(sentences, return_tensors="pt", padding=True)
 
         outputs = self.model(**inputs)
 
@@ -24,19 +32,14 @@ class SentenceEmbedder():
 
 
 if __name__ == "__main__":
-    from transformers import AutoModel, AutoTokenizer
+    from seat import SEAT6, SEAT7, SEAT8
 
     model_name = 'distilbert-base-uncased'
-    data_filename = 'data/sent-weat6.jsonl'
-
-    model = AutoModel.from_pretrained(model_name)
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-    embedder = SentenceEmbedder(model, tokenizer)
+    
+    embedder = SentenceEmbedder(model_name)
 
     seat_metrics = {"SEAT6": SEAT6(), "SEAT7": SEAT7(), "SEAT8": SEAT8()}
 
     for name in seat_metrics:
         value = seat_metrics[name](embedder)
-
         print(f"{name}: {value}")
