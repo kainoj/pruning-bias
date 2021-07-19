3acdebeda0193bdabb5a26f16c159c338e4a7078
Przmek <kainoj@users.noreply.github.com>
2021-07-19 20:45:39 +0900

    legititimate implementation of WEAT

---
+++ weat.py
@@ -0,0 +1,108 @@
+import json
+
+import torch
+import torch.nn as nn
+from typing import Any, Callable  # Tuple, List
+
+
+class WEAT():
+
+    def __init__(self, data_filename: str) -> None:
+        """TODO
+        """
+        self.target_x, self.target_y, self.attribute_a, self.attribute_b = \
+            self._get_data(data_filename)
+
+    def _get_data(self, data_filename: str):
+        """ASd"""
+        with open(data_filename) as f:
+            data = json.load(f)
+
+        target_x = data['targ1']['examples']
+        target_y = data['targ2']['examples']
+        attribute_a = data['attr1']['examples']
+        attribute_b = data['attr2']['examples']
+
+        return target_x, target_y, attribute_a, attribute_b
+
+    def association(
+        self, w: torch.tensor, attribute_a: torch.tensor, attribute_b: torch.tensor
+    ) -> float:
+        """Association of a word with an attribute. Torch version.
+        """
+        _w = w.unsqueeze(0)
+        assoc_a = nn.CosineSimilarity(dim=1)(_w, attribute_a)
+        assoc_b = nn.CosineSimilarity(dim=1)(_w, attribute_b)
+        return assoc_a.mean() - assoc_b.mean()
+
+    def weat(self, target_x, target_y, attribute_a, attribute_b) -> float:
+        sum_x = sum([self.association(x, attribute_a, attribute_b) for x in target_x])
+        sum_y = sum([self.association(y, attribute_a, attribute_b) for y in target_y])
+        return sum_x - sum_y
+
+    def weat_effect_size(
+        self,
+        target_x: torch.tensor,
+        target_y: torch.tensor,
+        attribute_a: torch.tensor,
+        attribute_b: torch.tensor,
+    ) -> float:
+
+        target_xy = torch.vstack((target_x, target_y))
+
+        assoc_x = [self.association(x, attribute_a, attribute_b) for x in target_x]
+        assoc_y = [self.association(y, attribute_a, attribute_b) for y in target_y]
+        assoc_xy = [self.association(xy, attribute_a, attribute_b) for xy in target_xy]
+
+        assoc_x = torch.tensor(assoc_x)
+        assoc_y = torch.tensor(assoc_y)
+        assoc_xy = torch.tensor(assoc_xy)
+
+        return (assoc_x.mean() - assoc_y.mean()) / assoc_xy.std()
+
+    def __call__(self, embedder: Callable, *args: Any, **kwds: Any) -> float:
+
+        x_emb = embedder(self.target_x)
+        y_emb = embedder(self.target_y)
+        a_emb = embedder(self.attribute_a)
+        b_emb = embedder(self.attribute_b)
+
+        return self.weat_effect_size(x_emb, y_emb, a_emb, b_emb)
+
+
+class SentenceEmbedder():
+
+    def __init__(self, model, tokenizer, mode='CLS') -> None:
+
+        self.model = model
+        self.tokenizer = tokenizer
+
+        if mode != 'CLS':
+            raise NotImplementedError()
+
+    def __call__(self, x) -> torch.tensor:
+
+        inputs = self.tokenizer(x, return_tensors="pt", padding=True)
+
+        outputs = self.model(**inputs)
+
+        # Take the embedding of CLS token as sentence representation
+        return outputs.last_hidden_state[:, 0, :]
+
+
+if __name__ == "__main__":
+    from transformers import AutoModel, AutoTokenizer
+
+    model_name = 'distilbert-base-uncased'
+    data_filename = 'data/sent-weat6.jsonl'
+
+    model = AutoModel.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    embedder = SentenceEmbedder(model, tokenizer)
+
+    weat = WEAT(data_filename)
+
+    effect_size = weat(embedder)
+
+    print(f"effect size: {effect_size}")
