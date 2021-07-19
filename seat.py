91f582d1ed9b0541e7190ba25f513577dee2961c
Przmek <kainoj@users.noreply.github.com>
2021-07-19 21:38:57 +0900

    docstringed seat.py

--- seat.py
+++ seat.py
@@ -1,5 +1,12 @@
 from weat import WEAT
 
+"""These are SEAT score wrappers, as proposed by May et al, 2019:
+"On Measuring Social Biases in Sentence Encoders"
+
+Ref. https://arxiv.org/abs/1903.10561
+
+SEAT is just a WEAT that takes representation of the whole sentence.
+"""
 
 class SEAT6(WEAT):
 
