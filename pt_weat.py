8738ac1aa15081f90e18f3f49d51601abf9fcd28
Przmek <kainoj@users.noreply.github.com>
2021-07-19 15:40:38 +0900

    flaked8'd

--- pt_weat.py
+++ pt_weat.py
@@ -1,7 +1,4 @@
 from typing import List
-import numpy as np
-import scipy
-import json
 
 import torch
 from torch import nn
@@ -10,7 +7,9 @@ from transformers import AutoModel, AutoTokenizer
 from weat import get_data
 
 
-def association(w, attribute_a: torch.tensor, attribute_b: torch.tensor) -> float:
+def association(
+    w: torch.tensor, attribute_a: torch.tensor, attribute_b: torch.tensor
+) -> float:
     """Association of a word with an attribute. Torch version.
     """
     _w = w.unsqueeze(0)
@@ -18,12 +17,12 @@ def association(w, attribute_a: torch.tensor, attribute_b: torch.tensor) -> floa
     assoc_b = nn.CosineSimilarity(dim=1)(_w, attribute_b)
     return assoc_a.mean() - assoc_b.mean()
 
+
 def weat(target_x, target_y, attribute_a, attribute_b) -> float:
-    """
-    """    
     sum_x = sum([association(x, attribute_a, attribute_b) for x in target_x])
     sum_y = sum([association(y, attribute_a, attribute_b) for y in target_y])
     return sum_x - sum_y
+
 
 def weat_effect_size(
     target_x: torch.tensor,
@@ -34,21 +33,26 @@ def weat_effect_size(
 
     target_xy = torch.vstack((target_x, target_y))
 
-    assoc_x = torch.tensor([association(x, attribute_a, attribute_b) for x in target_x])
-    assoc_y = torch.tensor([association(y, attribute_a, attribute_b) for y in target_y])
-    assoc_xy = torch.tensor([association(xy, attribute_a, attribute_b) for xy in target_xy])
+    assoc_x = [association(x, attribute_a, attribute_b) for x in target_x]
+    assoc_y = [association(y, attribute_a, attribute_b) for y in target_y]
+    assoc_xy = [association(xy, attribute_a, attribute_b) for xy in target_xy]
+
+    assoc_x = torch.tensor(assoc_x)
+    assoc_y = torch.tensor(assoc_y)
+    assoc_xy = torch.tensor(assoc_xy)
 
     return (assoc_x.mean() - assoc_y.mean()) / assoc_xy.std()
 
 
 def get_representation(sentences: List[str], tokenizer, model) -> torch.tensor:
-    
+
     inputs = tokenizer(sentences, return_tensors="pt", padding=True)
     # outputs shape is (#sentences, #tokens, dim)
     outputs = model(**inputs).last_hidden_state
 
     # Take the embedding of CLS token as sentence representation
     return outputs[:, 0, :]
+
 
 def main():
     model_name = 'distilbert-base-uncased'
@@ -60,7 +64,8 @@ def main():
     target_x, target_y, attribute_a, attribute_b = get_data(data_filename)
 
     x_repr, y_repr, a_repr, b_repr = [
-        get_representation(dat, tokenizer, model) for dat in [target_x, target_y, attribute_a, attribute_b]
+        get_representation(dat, tokenizer, model)
+        for dat in [target_x, target_y, attribute_a, attribute_b]
     ]
 
     print(
