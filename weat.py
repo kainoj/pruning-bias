dbae8d7ec931f9bd8c9d8a3c25c6a752e059e592
Przmek <kainoj@users.noreply.github.com>
2021-07-19 21:33:49 +0900

    Docstringed and refactored weat.py

--- weat.py
+++ weat.py
@@ -2,19 +2,36 @@ import json
 
 import torch
 import torch.nn as nn
-from typing import Any, Callable  # Tuple, List
+from typing import Callable, List, Tuple
 
 
 class WEAT():
+    """Word Embedding Association Test (WEAT).
+
+    Based on:
+        Caliskan et al, 2017, "Semantics derived automatically from language
+        corpora contain human-like biases"
+
+    Ref.: https://www.cs.bath.ac.uk/~jjb/ftp/CaliskanEtAl-authors-full.pdf
+    """
 
     def __init__(self, data_filename: str) -> None:
-        """TODO
+        """WEAT
+
+        Args:
+            data_filename: path to .jsonl file containing test data.
         """
         self.target_x, self.target_y, self.attribute_a, self.attribute_b = \
             self._get_data(data_filename)
 
-    def _get_data(self, data_filename: str):
-        """ASd"""
+    def _get_data(self, data_filename: str) -> Tuple[List[str], List[str], List[str], List[str]]:
+        """Load test data
+        
+        Args:
+            data_filename: path to .jsonl file containing test data.
+
+        Retruns: two lists of targets and two lists of attributes.
+        """
         with open(data_filename) as f:
             data = json.load(f)
 
@@ -25,28 +42,42 @@ class WEAT():
 
         return target_x, target_y, attribute_a, attribute_b
 
-    def association(
-        self, w: torch.tensor, attribute_a: torch.tensor, attribute_b: torch.tensor
+    def association(self,
+        w: torch.tensor,
+        attribute_a: torch.tensor,
+        attribute_b: torch.tensor
     ) -> float:
-        """Association of a word with an attribute. Torch version.
-        """
+        """Differencial association of a word `w` with sets of attributes."""
+
         _w = w.unsqueeze(0)
         assoc_a = nn.CosineSimilarity(dim=1)(_w, attribute_a)
         assoc_b = nn.CosineSimilarity(dim=1)(_w, attribute_b)
         return assoc_a.mean() - assoc_b.mean()
 
-    def weat(self, target_x, target_y, attribute_a, attribute_b) -> float:
+    def weat(self, 
+        target_x: torch.tensor,
+        target_y: torch.tensor,
+        attribute_a: torch.tensor,
+        attribute_b: torch.tensor
+    ) -> float:
+        """Differential association of 2 sets of target words with the attributes"""
+
         sum_x = sum([self.association(x, attribute_a, attribute_b) for x in target_x])
         sum_y = sum([self.association(y, attribute_a, attribute_b) for y in target_y])
         return sum_x - sum_y
 
-    def weat_effect_size(
+    def effect_size(
         self,
         target_x: torch.tensor,
         target_y: torch.tensor,
         attribute_a: torch.tensor,
-        attribute_b: torch.tensor,
+        attribute_b: torch.tensor
     ) -> float:
+        """Measures the effect size.
+        
+        Effect size is "is a normalized measure of how separated the two
+        distributions (of associations between the target and attribute) are".
+        """
 
         target_xy = torch.vstack((target_x, target_y))
 
@@ -60,11 +91,18 @@ class WEAT():
 
         return (assoc_x.mean() - assoc_y.mean()) / assoc_xy.std()
 
-    def __call__(self, embedder: Callable, *args: Any, **kwds: Any) -> float:
+    def __call__(self, embedder: Callable) -> float:
+        """WEAT effect size.
 
+        Args:
+            embedder: anything that takes a list of sentences (strings)
+                and retruns their embeddigs
+
+        Returns: effect size of WEAT.
+        """
         x_emb = embedder(self.target_x)
         y_emb = embedder(self.target_y)
         a_emb = embedder(self.attribute_a)
         b_emb = embedder(self.attribute_b)
 
-        return self.weat_effect_size(x_emb, y_emb, a_emb, b_emb)
+        return self.effect_size(x_emb, y_emb, a_emb, b_emb)
