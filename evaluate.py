ec348d38f971e007b68a942b662188acefe1402c
Przmek <kainoj@users.noreply.github.com>
2021-07-20 09:47:48 +0900

    all models eval

--- evaluate.py
+++ evaluate.py
@@ -8,15 +8,15 @@ from src.metrics.seat import SEAT6, SEAT7, SEAT8
 @hydra.main(config_path="configs", config_name="eval")
 def evaluate(cfg: DictConfig) -> None:
     
-    model_name = cfg.models[0]
-    
-    embedder = SentenceEmbedder(model_name)
-
     seat_metrics = {"SEAT6": SEAT6(), "SEAT7": SEAT7(), "SEAT8": SEAT8()}
+      
+    for model_name in cfg.models:
+        print(f"Evaluating: '{model_name}'")
+        embedder = SentenceEmbedder(model_name)
 
-    for name in seat_metrics:
-        value = seat_metrics[name](embedder)
-        print(f"{name}: {value}")
+        for name in seat_metrics:
+            value = seat_metrics[name](embedder)
+            print(f"{name}: {value}")
 
 
 if __name__ == "__main__":
