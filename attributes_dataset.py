390f23582ffd0cae6e6c3e64e0ac221a1622c74b
Przmek <kainoj@users.noreply.github.com>
2021-07-21 23:36:10 +0900

    downlad and extracrt dataset

--- attributes_dataset.py
+++ attributes_dataset.py
@@ -9,13 +9,16 @@ from torch.utils.data import Dataset
 
 class AttributesDataset(Dataset):
 
-    news_data_fname = 'news-commentary-v15.en.gz'
+    news_data_zip = 'news-commentary-v15.en.gz'
+    news_data_txt = 'news-commentary-v15.en.txt'
     news_data_url = 'http://data.statmt.org/news-commentary/v15/training-monolingual/news-commentary-v15.en.gz'
 
     def __init__(self, cache_dir='~/cache') -> None:
         super().__init__()
 
         self.cache_dir = self._prepare_cache_dir(cache_dir)
+        self.data_zip_path = self.cache_dir / self.news_data_zip
+        self.data_txt_path = self.cache_dir / self.news_data_txt
         
         self.prepare_data()
 
@@ -26,19 +29,15 @@ class AttributesDataset(Dataset):
 
     def prepare_data(self):
 
-        data_fpath = self.cache_dir / self.news_data_fname
-
-        if not data_fpath.exists():
-            print(f'Dowloading data into {data_fpath}.')
-            urllib.request.urlretrieve(self.news_data_url, data_fpath)
-        else:
-            print('Data exist, no need to download.')
-
-        print(data_fpath)
-        # with gzip.open(data_fpath, 'rt') as f_in:
-        #     with gzip.open('/home/joe/file.txt.gz', 'wt') as f_out:
-        #         shutil.copyfileobj(f_in, f_out)
+        if not self.data_zip_path.exists():
+            print(f'Dowloading data into {self.data_zip_path}')
+            urllib.request.urlretrieve(self.news_data_url, self.data_zip_path)
         
+        if not self.data_txt_path.exists():
+            print(f'Extracting data to {self.data_txt_path}')
+            with gzip.open(self.data_zip_path, 'rt') as f_in:
+                with open(self.data_txt_path, 'wt') as f_out:
+                    shutil.copyfileobj(f_in, f_out)
 
 
 if __name__ == "__main__":
