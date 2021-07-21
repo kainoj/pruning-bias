97b857c2aaa4e7af748d7f525011ac166b606d02
Przmek <kainoj@users.noreply.github.com>
2021-07-21 22:08:01 +0900

    lil refactor:

--- extract.py
+++ extract.py
@@ -1,20 +1,23 @@
 import regex as re
 
+def get_attribute_set(filepath: str) -> set:
+    """Reads file with attributes and returns a set containing them all"""
+    with open(filepath) as f:
+        return {l.strip() for l in f.readlines()}
+
+
 def main():
     data_filepath = 'data/news-commentary-v15.en'
     female_attributes_filepath = 'data/female.txt'
     male_attributes_filepath = 'data/male.txt'
     stereotypes_filepath = 'data/stereotype.txt'
 
-    with open(female_attributes_filepath) as f:
-        female_attr = {l.strip() for l in f.readlines()}
-
-    with open(male_attributes_filepath) as f:
-        male_attr = {l.strip() for l in f.readlines()}
-
-    with open(stereotypes_filepath) as f:
-        stereo_attr = {l.strip() for l in f.readlines()}
+    female_attr = get_attribute_set(female_attributes_filepath)
+    male_attr = get_attribute_set(male_attributes_filepath)
+    stereo_attr = get_attribute_set(stereotypes_filepath)
     
+    # This regexp basically tokenizes a sentence over spaces and 's, 're, 've..
+    # It's originally taken from OpenAI's GPT-2 Encoder implementation
     pat = re.compile(r"""'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""")
 
     male_cntr = 0
