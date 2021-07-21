918f481202e130469e9eceb156efcd21755e29fb
Przmek <kainoj@users.noreply.github.com>
2021-07-21 23:48:03 +0900

    download extract pipeline

--- attributes_dataset.py
+++ attributes_dataset.py
@@ -17,10 +17,7 @@ class AttributesDataset(Dataset):
         super().__init__()
 
         self.cache_dir = self._prepare_cache_dir(cache_dir)
-        self.data_zip_path = self.cache_dir / self.news_data_zip
-        self.data_txt_path = self.cache_dir / self.news_data_txt
-        
-        self.prepare_data()
+        self.data_txt_path = self.prepare_data()
 
     def _prepare_cache_dir(self, cache_dir) -> Path:
         _cache_dir = Path(cache_dir).expanduser() / 'bs-data'
@@ -29,15 +26,25 @@ class AttributesDataset(Dataset):
 
     def prepare_data(self):
 
-        if not self.data_zip_path.exists():
-            print(f'Dowloading data into {self.data_zip_path}')
-            urllib.request.urlretrieve(self.news_data_url, self.data_zip_path)
+        data_zip_path = self.cache_dir / self.news_data_zip
+        data_txt_path = self.cache_dir / self.news_data_txt
+
+        if not data_zip_path.exists():
+            print(f'Dowloading data into {data_zip_path}')
+            urllib.request.urlretrieve(self.news_data_url, data_zip_path)
         
-        if not self.data_txt_path.exists():
-            print(f'Extracting data to {self.data_txt_path}')
-            with gzip.open(self.data_zip_path, 'rt') as f_in:
-                with open(self.data_txt_path, 'wt') as f_out:
+        if not data_txt_path.exists():
+            print(f'Extracting data to {data_txt_path}')
+            with gzip.open(data_zip_path, 'rt') as f_in:
+                with open(data_txt_path, 'wt') as f_out:
                     shutil.copyfileobj(f_in, f_out)
+        
+        return data_txt_path
+    
+    def extract_data(self):
+        # if not cached -> extract and cache
+        # if cached -> retrun cached
+        pass
 
 
 if __name__ == "__main__":
