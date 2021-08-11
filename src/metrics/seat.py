81cf4cb3ac24051f37d072df04ce92fbf6e6162a
Przmek <kainoj@users.noreply.github.com>
2021-08-11 20:38:34 +0900

    updated paths in SEAT*

--- src/metrics/seat.py
+++ src/metrics/seat.py
@@ -1,4 +1,6 @@
 from src.metrics.weat import WEAT
+from pathlib import Path
+import hydra
 
 """These are SEAT score wrappers, as proposed by May et al, 2019:
 "On Measuring Social Biases in Sentence Encoders"
@@ -6,24 +8,23 @@ from src.metrics.weat import WEAT
 Ref. https://arxiv.org/abs/1903.10561
 
 SEAT is just a WEAT that takes representation of the whole sentence.
-
-TODO(przemek): `../../../` in the path is a quickfix that escapes hydra
-    workdir (`outputs/data/hour). Probably I want to move data somewhere else.
 """
+
+cwd = Path(hydra.utils.get_original_cwd())
 
 class SEAT6(WEAT):
 
     def __init__(self) -> None:
-        super().__init__(data_filename='../../../data/sent-weat6.jsonl')
+        super().__init__(data_filename=cwd / 'data/sent-weat6.jsonl')
 
 
 class SEAT7(WEAT):
 
     def __init__(self) -> None:
-        super().__init__(data_filename='../../../data/sent-weat7.jsonl')
+        super().__init__(data_filename=cwd / 'data/sent-weat7.jsonl')
 
 
 class SEAT8(WEAT):
 
     def __init__(self) -> None:
-        super().__init__(data_filename='../../../data/sent-weat8.jsonl')
+        super().__init__(data_filename=cwd / 'data/sent-weat8.jsonl')
