c4a9916a9d52201e9964a875acecff918af31be8
Przmek <kainoj@users.noreply.github.com>
2021-08-11 20:37:34 +0900

    updated evaluation script

--- evaluate.py
+++ evaluate.py
@@ -1,18 +1,32 @@
 import hydra
-from omegaconf import DictConfig, OmegaConf
+import torch
+from omegaconf import DictConfig
 
-from src.utils.sentence_embbeder import SentenceEmbedder
-from src.metrics.seat import SEAT6, SEAT7, SEAT8
+from src.models.modules.mlm_pipeline import Pipeline
+from src.models.modules.tokenizer import Tokenizer
 
 
 @hydra.main(config_path="configs", config_name="eval")
 def evaluate(cfg: DictConfig) -> None:
+    from src.metrics.seat import SEAT6, SEAT7, SEAT8
     
     seat_metrics = {"SEAT6": SEAT6(), "SEAT7": SEAT7(), "SEAT8": SEAT8()}
+    device = 'cuda:0'  # move to config
       
     for model_name in cfg.models:
         print(f"Evaluating: '{model_name}'")
-        embedder = SentenceEmbedder(model_name)
+
+        pipeline = Pipeline(model_name, embedding_layer='last').to(device)
+        tokenizer = Tokenizer(model_name)
+
+        # A quick workaround, we need something callable
+        #   which takes str and returns an embedding.
+        def embbedder_fn(sentence):
+            with torch.no_grad():
+                tknzd = tokenizer(sentence).to(device)
+                return pipeline(tknzd, embedding_layer='CLS')
+
+        embedder = embbedder_fn
 
         for name in seat_metrics:
             value = seat_metrics[name](embedder)
@@ -21,5 +35,3 @@ def evaluate(cfg: DictConfig) -> None:
 
 if __name__ == "__main__":
     evaluate()
-
-    
