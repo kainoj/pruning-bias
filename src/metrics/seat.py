b32b7f7fb6b06397e3ab93ce3d9fa3aa196a42a1
Przmek <kainoj@users.noreply.github.com>
2021-08-13 18:40:31 +0900

    wrapped seat with weat

--- src/metrics/seat.py
+++ src/metrics/seat.py
@@ -1,31 +1,12 @@
 from src.metrics.weat import WEAT
-from pathlib import Path
-import hydra
-
-"""These are SEAT score wrappers, as proposed by May et al, 2019:
-"On Measuring Social Biases in Sentence Encoders"
-
-Ref. https://arxiv.org/abs/1903.10561
-
-SEAT is just a WEAT that takes representation of the whole sentence.
-"""
-
-cwd = Path(hydra.utils.get_original_cwd())
 
 
-class SEAT6(WEAT):
+class SEAT(WEAT):
+    """Sentence Embedding Association Test (SEAT).
 
-    def __init__(self) -> None:
-        super().__init__(data_filename=cwd / 'data/sent-weat6.jsonl')
+    SEAT is just a WEAT that takes representation of the whole sentence、for
+    example an embedding of [CLS] token.
+    """
 
-
-class SEAT7(WEAT):
-
-    def __init__(self) -> None:
-        super().__init__(data_filename=cwd / 'data/sent-weat7.jsonl')
-
-
-class SEAT8(WEAT):
-
-    def __init__(self) -> None:
-        super().__init__(data_filename=cwd / 'data/sent-weat8.jsonl')
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
